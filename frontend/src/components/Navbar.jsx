06d83244c01b6c7e8a454f8acf3cfd1586ac6853
Diana <20230019@uthh.edu.mx>
2026-02-08 10:28:31 -0600

    Avances frontend: navbar, register, estructura base

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,17 +1,20 @@
 import "./Navbar.css";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <nav className="navbar">
       <h2 className="logo">Nova Graf</h2>
 
-      <ul>
-        <li>Inicio</li>
-        <li>Catálogo</li>
-        <li>Nosotros</li>
-        <li>Contacto</li>
-        <li>Login</li>
-      </ul>
+    <ul>
+      <li><Link to="/">Inicio</Link></li>
+      <li><Link to="/catalogo">Catálogo</Link></li>
+      <li><Link to="/nosotros">Nosotros</Link></li>
+      <li><Link to="/contacto">Contacto</Link></li>
+      <li><Link to="/register">Registro</Link></li>
+      <li><Link to="/login">Login</Link></li>
+    </ul>
+
     </nav>
   );
 }
