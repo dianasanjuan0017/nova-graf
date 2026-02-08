40b641867055a7b27c3a55d9d1c4771c85577c91
Diana <20230019@uthh.edu.mx>
2026-02-08 11:49:52 -0600

    registro

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const cors = require("cors");   
 const app = express();
 
 app.use(express.json());
+app.use(cors());                
 
 const userRoutes = require("./routes/user.routes");
 app.use("/api/users", userRoutes);
@@ -14,7 +16,6 @@ app.use("/api/catalog", catalogRoutes);
 
 const productRoutes = require("./routes/product.routes");
 app.use("/api/products", productRoutes);
-
 
 app.listen(3000, () => {
   console.log("Servidor corriendo en puerto 3000");
